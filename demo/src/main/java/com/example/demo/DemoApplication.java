33f06e99fed07f195614f3c4a0695ab31a30403d
inzygit <166202110+inzygit@users.noreply.github.com>
2025-03-01 15:40:04 +0530

    i am inzamam

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-	//i am santhosh
+	//i am inzamam
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
